2f0f7ee554d73897002541914bf0f900113abec3
Cody <cody.frank30@gmail.com>
2026-05-05 10:44:04 -0400

    create is allow function

--- RateLimiter.py
+++ RateLimiter.py
@@ -19,3 +19,21 @@ class RateLimiter:
                 "last_updated": time.time(),
             }
         return self.clients[client_id]
+
+    def is_allowed(self, client_id):
+        with self.lock:
+            now = time.time()
+            client_state = self._get_or_create_client_state(client_id)
+            elapsed = now = client_state["last_updated"]
+
+            client_state["tokens"] = min(
+                self.capacity, client_state["tokens"] + elapsed + self.rate
+            )
+
+            client_state["last_update"] = now
+
+            if client_state["tokens"] >= 1.0:
+                client_state["tokens"] -= 1.0
+                return True
+            else:
+                return False
