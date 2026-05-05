90dbc5048cc3eb1b80622d3df3f6757b8ff43a01
Cody <cody.frank30@gmail.com>
2026-05-05 10:37:20 -0400

    create class init

--- RateLimiter.py
+++ RateLimiter.py
@@ -1,4 +1,13 @@
+import threading
+import time
+
+
 class RateLimiter:
-    def __init__(self, max_requests, window_seconds):
-        self.max_requests = max_requests
-        self.window_seconds = window_seconds
+    def __init__(self, rate, capacity):
+        if rate <= 0 or capacity <= 0:
+            raise ValueError("Rate and Capacity must both be positive")
+
+        self.rate = rate
+        self.capacity = capacity
+        self.clients = {}
+        self.lock = threading.Lock()
