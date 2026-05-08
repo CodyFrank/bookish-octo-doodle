6c54071b462878c279d5ed455db799a82dafc030
Cody <cody.frank30@gmail.com>
2026-05-08 15:23:42 -0400

    updating RateLimiter.ts

--- RateLimiter.ts
+++ RateLimiter.ts
@@ -1,22 +1,39 @@
+interface ClientState {
+	tokens: number;
+	lastUpdated: number;
+};
+interface Tokens {
+	clientId: ClientState;
+};
+
 class RateLimiter {
-	private rate: number;
-	private capacity: number;
-	tokens: object;
+	private readonly rate: number;
+	private readonly capacity: number;
+	private tokens: Tokens;
 	constructor(rate: number, capacity: number) {
 		this.rate = rate;
 		this.capacity = capacity;
 		this.tokens = {}
-	}
+	};
 
-	_get_or_create_client_state(clinet_id: string, time: number): object {
-		return {}
-	}
+	private _getOrCreateClientState(clientId: string, time: number): ClientState {
+		let value = this.tokens.get()
+		if (this.tokens.clientId) {
+			k
+		}
+
+		if (this.tokens[clientId])
+			return clientState
+	};
 
 	isAllowed(clinet_id: string): boolean {
 		// need to lock threads
 		// get current time
+		const now = Date.now();
 		// get client state
+		let clientState = this._getOrCreateClientState(clinet_id, now);
 		// get elapesed time
+		const elapsed = now - clientState.last_updated;
 		// calculate tokens based on smallest of capacity or state.tokens + elapsed + rate
 		// store updated token value
 		//
